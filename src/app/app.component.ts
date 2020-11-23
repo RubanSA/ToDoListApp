f814314d532d324753e53c97cd8997f85ecbe900
Сергей Рубан <ruban@kr.step>
2020-11-23 19:53:43 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {ToDoListItem} from "../app/to-do-list-item"
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  //template: "<div>Hello world!</div>",
+  styleUrls: ['./app.component.css']
+  //styles: ["div{color: red}"]
+})
+export class AppComponent {
+  title = 'ToDoList Angular App ';
+  getTitle = function ():string {
+    return this.title;
+  }
+}
